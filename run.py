0645a538ee8a69bbecd58fdd7bf3fac726999a3e
jbwexler <jbwexler@tutanota.com>
2024-10-08 14:47:09 -0700

    many updates

--- run.py
+++ run.py
@@ -8,23 +8,27 @@ import smtplib
 import sys
 from email.mime.text import MIMEText
 from email.mime.multipart import MIMEMultipart
-from datetime import datetime
+from datetime import datetime, date, timedelta
+import time
 import pandas as pd
+import re
 from flywheel import (
     ProjectOutput,
     AcquisitionListOutput,
     FileListOutput,
     Gear
 )
+from heudiconv.heuristics.reproin import parse_series_spec
 
 pip.main(["install", "--upgrade", "git+https://github.com/poldracklab/wbhi-utils.git"])
 from wbhiutils import parse_dicom_hdr
 from wbhiutils.constants import (
-    REDCAP_API_URL,
-    DATE_FORMAT_FW
+    ADMIN_EMAIL
 )
 
 log = logging.getLogger(__name__)
+
+WAIT_TIMEOUT = 3600 * 2
 
 DATAVIEW_COLUMNS = [
     'subject.id',
@@ -37,6 +41,7 @@ DATAVIEW_COLUMNS = [
     'file.classification.Intent',
     'file.classification.Measurement',
     'file.classification.Features',
+    'file.modality',
     'file.file_id',
     'file.tags',
     'file.type',
@@ -102,9 +107,9 @@ def get_subjects(project: ProjectOutput) -> pd.DataFrame:
     file_df = create_view_df(project, DATAVIEW_COLUMNS) 
     file_df = file_df[file_df['file.type'].isin(('dicom', 'nifti'))]
     
-    # Skip subject if all sessions have been bidsified
+    # Skip subject if all sessions have a 'bidsified' tag
     file_df = file_df.groupby('subject.id').filter(
-        lambda x: x['session.info.BIDS'].isna().any()
+            lambda x: x['session.tags'].apply(lambda x: 'bidsified' not in x).any()
     )
     nonbids_df = file_df.copy()
     
@@ -127,51 +132,77 @@ def get_subjects(project: ProjectOutput) -> pd.DataFrame:
                     f"\n{filt_out_set}"
         )
 
+    if file_df.empty:
+        log.info("No subjects were found that need to be bidsified.")
+        sys.exit(0)
+
     return file_df
 
 
 def classify(file_df: pd.DataFrame) -> pd.DataFrame:
     """Determines which acquisitions should be bidsified and converts the acquisition labels
-    to repronim standard. For acquisitions that won't be bidsified, adds '_ignore_BIDS' to
+    to repronim standard. For acquisitions that won't be bidsified, adds '_ignore-BIDS' to
     the end of the acquisition label, signaling the bids-curate gear to skip."""
-    
+
     def reproin_filter(row: pd.Series) -> str:
         measurement = row['file.classification.Measurement']
         intent = row['file.classification.Intent']
         label = row['acquisition.label']
+        label = re.sub(r'_\d$', '', label)
+        
+        validator = parse_series_spec(label)
+        reproin = None
 
         if 'bids-gear-ignore'in row['file.tags']:
             # Above tag can be used to mark files that can't be niftified and should be ignored
             return None
+        elif '_ignore-BIDS' in label:
+            return None
+        elif label.startswith('GOBRAIN_'):
+            return row['acquisition.label'] + '_ignore-BIDS'
+        elif intent and 'Localizer' in intent:
+            return row['acquisition.label'] + '_ignore-BIDS'
         elif measurement and 'Perfusion' in measurement:
-            return 'asl'
+            reproin = 'asl'
         elif measurement and intent and 'Structural' in intent:
             if 'T1' in measurement:
-                return 'anat-T1w'
+                reproin = 'anat-T1w'
             elif 'T2' in measurement:
-                return 'anat-T2w'   
+                reproin = 'anat-T2w'   
             elif 'Diffusion' in measurement:
-                return 'dwi'
+                reproin = 'dwi'
         elif (intent and 'Functional' in intent and
-            label and 'rest' in label):
-            return 'func_task-rest'
-        elif measurement and 'Fieldmap' in measurement:
-            return 'fmap'
-        else:
-            return label + '_ignore_BIDS'
+            label and 'rest' in label.lower()):
+            reproin = 'func_task-rest'
+        elif intent and 'Fieldmap' in intent:
+            reproin = 'fmap'
+        
+        if reproin:
+            if validator:
+                return label
+            return reproin
+        
+        # Use the pre-regex acq-label to avoid duplicate filenames since runs won't be added
+        return row['acquisition.label'] + '_ignore-BIDS'
 
     classified_df = file_df.copy()
-    classified_df = classified_df[classified_df['session.info.BIDS'].isna()]
+    breakpoint()
+    classified_df = classified_df[classified_df['session.tags'].apply(lambda x: 'bidsified' not in x)]
 
     reproin_series = classified_df.apply(reproin_filter, axis=1)
-    if not reproin_series.empty:
-        classified_df['reproin'] = reproin_series
+    if reproin_series.empty:
+        log.info("No acquisitions were found that need to be bidsified.")
+        sys.exit(0)
+
+    classified_df['reproin'] = reproin_series
 
     # Remove subjects if any included acqs don't have corresponding niftis
     filenames_joined = ','.join(classified_df[
         classified_df['file.type'] == 'nifti']['file.name'].tolist())
     bidsify_df = classified_df.copy()
     bidsify_df = bidsify_df[~bidsify_df['reproin'].isna()]
+    bidsify_df = bidsify_df[bidsify_df['file.modality'] != 'SR']
+    bidsify_df = bidsify_df[~bidsify_df['reproin'].str.contains('_ignore-BIDS')]
     bidsify_df = bidsify_df[bidsify_df['file.type'] == 'dicom'].groupby('subject.id').filter(
         lambda x: x['file_root'].apply(
             lambda y: y in filenames_joined
@@ -203,7 +234,7 @@ def add_repronim(classified_df: pd.DataFrame) -> pd.DataFrame:
     def add_run(df: pd.DataFrame()):
         if df.shape[0] > 1:
             df = df.sort_values(by='acquisition.timestamp')
-            n_digits = len(str(df.shape[0]))
+            n_digits = max(2, len(str(df.shape[0])))
             padded_list = [str(n).zfill(n_digits) for n in range(1, df.shape[0] + 1)]
             df.insert(df.shape[1], 'run', padded_list)
             df['reproin'] = df['reproin'] + '_run-' + df['run']
@@ -211,10 +242,81 @@ def add_repronim(classified_df: pd.DataFrame) -> pd.DataFrame:
 
     repronim_df = repronim_df.groupby(['session.id', 'reproin']).apply(add_run).reset_index(drop=True)
 
-    for i, row in repronim_df.iterrows():
-        acq = client.get_acquisition(row['acquisition.id'])
-        acq.update({'label': row['reproin']})
+    for name, ses_df in repronim_df.groupby('session.id'):
+        for i, row in ses_df.iterrows():
+            if row['reproin'] != row['acquisition.label']:
+                acq_match_df = ses_df[ses_df['acquisition.label'] == row['reproin']]
+                if not acq_match_df.empty:
+                    acq_tmp = client.get_acquisition(acq_match_df['acquisition.id'].iloc[0])
+                    acq_tmp.update({'label': row['reproin'] + '_tmp'})
+                acq = client.get_acquisition(row['acquisition.id'])
+                acq.update({'label': row['reproin']})
+            
+    return repronim_df
 
+def rename_sessions(repronim_df: pd.DataFrame()) -> None:
+    """Renames session to '01', or the next available number if existing sessions."""
+    for sub_id in repronim_df['subject.id'].unique():
+        subject = client.get_subject(sub_id)
+        sub_sessions = subject.sessions()
+
+        if len(sub_sessions) == 1:
+            new_session_label = '01'
+            session = sub_sessions[0]
+            session.update({'label': new_session_label})
+        else:
+            sub_sessions_sorted = sorted(sub_sessions, key=lambda d: d.timestamp)
+            num_digits = len(str(len(sub_sessions_sorted)))
+            zero_pad = max(num_digits, 2)
+            for i, session in enumerate(sub_sessions_sorted, 1):
+                new_session_label = str(i).zfill(zero_pad)
+                session.update({'label': new_session_label})
+
+def submit_bids_jobs(repronim_df: pd.DataFrame()):
+    curate_bids_gear = client.lookup('gears/curate-bids')
+    for sub_id in repronim_df['subject.id'].unique():
+        subject = client.get_subject(sub_id)
+        run_gear(curate_bids_gear, {}, {}, subject)
+
+def wait_for_jobs(project: ProjectOutput) -> None:
+    query = f'parents.project={project.id},state=running,gear_info.name=curate-bids'
+    start_time = time.time()
+    while client.jobs.find(query):
+        if time.time() - start_time > WAIT_TIMEOUT:
+            log.error("Wait timeout for copy to complete")
+            sys.exit(1)
+        time.sleep(5)
+
+def tag_and_email(project: ProjectOutput) -> None:
+    cutoff = (datetime.now() - timedelta(hours=6)).isoformat()
+
+    failed_q = f'parents.project={project.id},state=failed,gear_info.name=curate-bids,created>{cutoff}'
+    failed_jobs = client.jobs.find(failed_q)
+    failed_job_subjects = [job.parents.subject for job in failed_jobs]
+    for sub_id in failed_job_subjects:
+        sub = client.get_subject(sub_id)
+        for ses in sub.sessions(): 
+            if 'bids-failed' not in ses.tags:
+                ses.add_tag('bids-failed')
+
+    complete_q = f'parents.project={project.id},state=complete,gear_info.name=curate-bids,created>{cutoff}'
+    complete_jobs = client.jobs.find(complete_q)
+    complete_job_subjects = [job.parents.subject for job in complete_jobs]
+    for sub_id in complete_job_subjects:
+        sub = client.get_subject(sub_id)
+        for ses in sub.sessions():
+            if 'bids-failed' in ses.tags:
+                ses.delete_tag('bids-failed')
+            if 'bidsified' not in ses.tags:
+                ses.add_tag('bids-failed')
+    
+    send_email(
+        'failed bids-curate jobs',
+        ', '.join(failed_job_subjects),
+        config["gmail_address"],
+        ADMIN_EMAIL,
+        config["gmail_password"]
+    )
 
 def send_email(subject, html_content, sender, recipients, password):
     msg = MIMEMultipart()
@@ -237,32 +339,23 @@ def run_gear(
     tags=None) -> str:
     """Submits a job with specified gear and inputs. dest can be any type of container
     that is compatible with the gear (project, subject, session, acquisition)"""
-
-    try:
-        # Run the gear on the inputs provided, stored output in dest constainer and returns job ID
-        gear_job_id = gear.run(inputs=inputs, config=config, destination=dest, tags=tags)
-        log.debug('Submitted job %s', gear_job_id)
-        return gear_job_id
-    except flywheel.rest.ApiException:
-        log.exception('An exception was raised when attempting to submit a job for %s', gear.name)
-
-def test_validate():
-    from bids_curate.flywheel_bids.supporting_files.bidsify_flywheel import process_matching_templates
-    from bids_curate.flywheel_bids.supporting_files.templates import load_template
-
-    template = load_template(template_name='reproin')
-    session = client.get_session('66a92ea0d0c712d5593d5c08')
-    context = {"container_type": "session"}  
-    process_matching_templates(session, template)
-    breakpoint()
-
+   
+    for i in range(0,3):
+        try:
+            # Run the gear on the inputs provided, stored output in dest constainer and returns job ID
+            gear_job_id = gear.run(inputs=inputs, config=config, destination=dest, tags=tags)
+            log.debug('Submitted job %s', gear_job_id)
+            return gear_job_id
+        except flywheel.rest.ApiException:
+            #log.exception('An exception was raised when attempting to submit a job for %s', gear.name)
+            time.sleep(1)
 def main():
     gtk_context.init_logging()
     gtk_context.log_config()
 
-    #test_validate()
     deid_project = client.lookup("joe_test/deid_joe")
     #deid_project = client.lookup("wbhi/deid")
+
     try:
         file_df = get_subjects(deid_project)
     except (KeyError, NameError):
@@ -271,6 +364,11 @@ def main():
 
     file_df = classify(file_df)
     file_df = add_repronim(file_df)
+    rename_sessions(file_df)
+    submit_bids_jobs(file_df)
+    wait_for_jobs(deid_project)
+    tag_and_email(deid_project)
+    breakpoint()
 
 if __name__ == "__main__":
     with flywheel_gear_toolkit.GearToolkitContext() as gtk_context:
